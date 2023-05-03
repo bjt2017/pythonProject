9b162fa513d2bccca4bc953e2c56245aa40be4d8
Benjamin Morawiec <morabenji3@gmail.com>
2023-05-03 15:43:57 +0200

    ajout du calque

---
+++ data/carte/spr_tileset_sunnysideworld_16px.tsx
@@ -0,0 +1,63 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="spr_tileset_sunnysideworld_16px" tilewidth="16" tileheight="16" tilecount="4096" columns="64">
+ <image source="../../../../PycharmProjects/pythonProject/data/assets/spr_tileset_sunnysideworld_16px.png" width="1024" height="1024"/>
+ <wangsets>
+  <wangset name="uuu" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="2118" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="2180" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="2181" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="2182" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="2183" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="2184" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="2244" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="2245" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="2246" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="2247" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="2248" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="2308" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="2309" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="2310" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="2311" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="2312" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="2373" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="2374" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="2375" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+  <wangset name="maison" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="577" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="578" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="579" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="580" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="581" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="582" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="641" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="643" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="644" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="645" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="646" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="647" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="776" wangid="0,0,0,1,0,0,0,0"/>
+  </wangset>
+  <wangset name="Toit" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+  </wangset>
+  <wangset name="nuage" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="1153" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="1154" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="1155" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="1156" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="1157" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="1158" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="1160" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="1217" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="1219" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="1220" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="1221" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="1222" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="1223" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
