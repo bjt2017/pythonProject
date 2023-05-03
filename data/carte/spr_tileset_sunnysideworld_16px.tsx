8ff188a43aefedbfac4873f60233d5b2fa574922
Benjamin Morawiec <morabenji3@gmail.com>
2023-05-03 16:07:27 +0200

    modif tiled

--- data/carte/spr_tileset_sunnysideworld_16px.tsx
+++ data/carte/spr_tileset_sunnysideworld_16px.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="spr_tileset_sunnysideworld_16px" tilewidth="16" tileheight="16" tilecount="4096" columns="64">
- <image source="../../../../PycharmProjects/pythonProject/data/assets/spr_tileset_sunnysideworld_16px.png" width="1024" height="1024"/>
+ <image source="../assets/spr_tileset_sunnysideworld_16px.png" width="1024" height="1024"/>
  <wangsets>
   <wangset name="uuu" type="corner" tile="-1">
    <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
